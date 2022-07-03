a015e0fa310968c4d60c8cccb18d0dc18e0e91ca
Luca Lorenzini <chielorenzi@gmail.com>
2022-07-04 01:02:05 +0200

    Init index and album page

---
+++ blog/pages/[id].js
@@ -0,0 +1,28 @@
+import { get } from 'lib/api.js'
+
+function Album ({ album }) {
+	return <p>Here an album {album.attributes.name}</p>
+}
+
+export async function getStaticPaths() {
+	const res = await get('/albums')
+	const albums = (await res.json()).data
+	
+	const paths = albums.map((album) => ({
+		params: { id: album.id.toString() },
+  	}))
+	
+  	// { fallback: false } means other routes should 404
+  	return { paths, fallback: false }
+}
+
+export async function getStaticProps({ params }) {
+  	const res = await get(`/albums/${params.id}`)
+  	const album = (await res.json()).data
+
+	console.log("Album:", album)
+
+  	return { props: { album } }
+}
+
+export default Album

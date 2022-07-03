d9cdb9bf206a440a1464e978e9f2ddd79104fd2c
Luca Lorenzini <chielorenzi@gmail.com>
2022-07-04 01:00:37 +0200

    Set Next.js base import folder

--- blog/next.config.js
+++ blog/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: 'build',
+  distDir: 'build', // folder where to place the build
 }
 
 module.exports = nextConfig

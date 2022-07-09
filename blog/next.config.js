2adf0b250d59817d63f9b966d3f0f1377f8f8214
Luca Lorenzini <chielorenz@gmail.com>
2022-07-09 19:22:16 +0200

    Enable unoptimized images

--- blog/next.config.js
+++ blog/next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	images: {
-		domains: ['localhost'],
+	experimental: {
+		images: {
+			unoptimized: true,
+		},
 	},
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

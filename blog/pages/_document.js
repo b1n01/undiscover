10aa724775b6b275fe2732e500128a80f66322bb
Luca Lorenzini <chielorenz@gmail.com>
2022-07-17 12:15:40 +0200

    Update favicon

--- blog/pages/_document.js
+++ blog/pages/_document.js
@@ -6,11 +6,11 @@ export default function Document() {
 	return (
 		<Html>
 			<Head>
-				{/* Custom font */}
 				<link
 					href="https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&display=swap"
 					rel="stylesheet"
 				/>
+				<link href="/favicon.svg" rel="favicon" type="image/svg+xml" />
 			</Head>
 			<body>
 				<Main />
